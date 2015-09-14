e3e57c5d371b0735b994f597b6ffb8b311673441
Liu Xinan <xinan@me.com>
2015-09-14 23:06:38 +0800

    Adjust buffer size

--- src/RequestHandlerThread.java
+++ src/RequestHandlerThread.java
@@ -8,16 +8,22 @@ import java.net.Socket;
  */
 public class RequestHandlerThread extends Thread {
 
-  private Socket clientSocket;
+  final private Socket clientSocket;
+  final private int BUFFER_SIZE;
 
   public RequestHandlerThread(Socket clientSocket) {
+    this(clientSocket, 65536);
+  }
+
+  public RequestHandlerThread(Socket clientSocket, int bufferSize) {
     this.clientSocket = clientSocket;
+    BUFFER_SIZE = bufferSize;
   }
 
   public void run() {
     try {
       System.out.printf("Connected to client: %s\n", clientSocket.getInetAddress());
-      byte[] buffer = new byte[1024];
+      byte[] buffer = new byte[BUFFER_SIZE];
       BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());
       PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
       int bytesRead;
