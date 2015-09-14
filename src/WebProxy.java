9efd6ac8d68f1589455c197a6fc82f3ff44a588c
Liu Xinan <xinan@me.com>
2015-09-15 04:28:25 +0800

    Tidy up `WebProxy`

--- src/WebProxy.java
+++ src/WebProxy.java
@@ -8,7 +8,6 @@ import java.net.SocketException;
 public class WebProxy {
   public static void main(String[] args) {
     try {
-      System.out.printf("Server starting...\n");
       int port = Integer.parseInt("1234");
       ServerSocket socket = new ServerSocket(port);
       Runtime.getRuntime().addShutdownHook(new Thread() {
@@ -17,18 +16,18 @@ public class WebProxy {
             System.out.printf("Server is shutting down...\n");
             socket.close();
           } catch (IOException e) {
-            System.out.printf("Error: %s\n", e.getMessage());
+            e.printStackTrace();
           }
         }
       });
-      System.out.printf("Listening on %d...\n", port);
+      System.out.printf("Server listening on %d...\n\n", port);
       while (!socket.isClosed()) {
         (new RequestHandlerThread(socket.accept())).start();
       }
     } catch (SocketException e) {
       System.out.printf("%s\n", e.getMessage());
     } catch (IOException e) {
-      System.out.printf("Error: %s\n", e.getMessage());
+      e.printStackTrace();
     } catch (IndexOutOfBoundsException | NumberFormatException  e) {
       System.out.printf("Invalid argument!\n");
     }
