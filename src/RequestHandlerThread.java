30cb13dda499a8dc50846ac12c89eb652c6f46b0
Liu Xinan <xinan@me.com>
2015-09-16 02:15:51 +0800

    Tidy up `RequestHandlerThread`

--- src/RequestHandlerThread.java
+++ src/RequestHandlerThread.java
@@ -21,38 +21,38 @@ public class RequestHandlerThread extends Thread {
 
   public void run() {
     try {
-      try {
-        HttpRequest clientRequest = new HttpRequest(clientSocket.getInputStream());
-        BufferedOutputStream clientOut = new BufferedOutputStream(clientSocket.getOutputStream(), BUFFER_SIZE);
-        System.out.printf("[%s] %s\n", clientSocket.getInetAddress(), clientRequest);
+      HttpRequest clientRequest = new HttpRequest(clientSocket.getInputStream());
+      BufferedOutputStream clientOut = new BufferedOutputStream(clientSocket.getOutputStream(), BUFFER_SIZE);
+      System.out.printf("[%s] %s\n", clientSocket.getInetAddress(), clientRequest);
 
-        Socket serverSocket = new Socket(clientRequest.getHost(), 80);
+      Socket serverSocket = new Socket(clientRequest.getHost(), 80);
 
-        BufferedInputStream serverIn = new BufferedInputStream(serverSocket.getInputStream(), BUFFER_SIZE);
-        BufferedOutputStream serverOut = new BufferedOutputStream(serverSocket.getOutputStream(), BUFFER_SIZE);
-        serverOut.write(clientRequest.toByteBuffer());
-        serverOut.flush();
+      BufferedInputStream serverIn = new BufferedInputStream(serverSocket.getInputStream(), BUFFER_SIZE);
+      BufferedOutputStream serverOut = new BufferedOutputStream(serverSocket.getOutputStream(), BUFFER_SIZE);
+      serverOut.write(clientRequest.toByteBuffer());
+      serverOut.flush();
 
-        int bytesRead;
-        byte[] buffer = new byte[BUFFER_SIZE];
-        while ((bytesRead = serverIn.read(buffer)) > 0) {
-          clientOut.write(buffer, 0, bytesRead);
-          clientOut.flush();
-        }
-        System.out.printf("Done. Active Thread: %d\r", Thread.activeCount());
-
-        serverSocket.close();
-        clientSocket.close();
-      } catch (SocketException e) {
-        System.out.printf("Client closed connection.\n");
-      } catch (IOException e) {
-        e.printStackTrace();
-      } catch (MalformedRequestException e) {
-        clientSocket.getOutputStream().write(e.getMessage().getBytes());
-        clientSocket.close();
+      int bytesRead;
+      byte[] buffer = new byte[BUFFER_SIZE];
+      while ((bytesRead = serverIn.read(buffer)) > 0) {
+        clientOut.write(buffer, 0, bytesRead);
+        clientOut.flush();
       }
+      System.out.printf("Done. Active Thread: %d\r", Thread.activeCount());
+
+      serverSocket.close();
+      clientSocket.close();
+    } catch (SocketException e) {
+      System.out.printf("Client closed connection.\n");
     } catch (IOException e) {
       e.printStackTrace();
+    } catch (MalformedRequestException e) {
+      try {
+        clientSocket.getOutputStream().write(e.getMessage().getBytes());
+        clientSocket.close();
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
     }
   }
 }
