f9c686f92cac82e94eaaddd3df5de0e7565e3f5e
Liu Xinan <xinan@me.com>
2015-09-16 12:01:53 +0800

    Handle `UnknownHostException`

--- src/RequestHandlerThread.java
+++ src/RequestHandlerThread.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  * Created by Xinan on 14/9/15.
@@ -21,38 +22,41 @@ public class RequestHandlerThread extends Thread {
 
   public void run() {
     try {
-      HttpRequest clientRequest = new HttpRequest(clientSocket.getInputStream());
-      BufferedOutputStream clientOut = new BufferedOutputStream(clientSocket.getOutputStream(), BUFFER_SIZE);
-      System.out.printf("[%s] %s\n", clientSocket.getInetAddress().getCanonicalHostName(), clientRequest);
-
-      Socket serverSocket = new Socket(clientRequest.getHost(), 80);
-
-      BufferedInputStream serverIn = new BufferedInputStream(serverSocket.getInputStream(), BUFFER_SIZE);
-      BufferedOutputStream serverOut = new BufferedOutputStream(serverSocket.getOutputStream(), BUFFER_SIZE);
-      serverOut.write(clientRequest.toByteBuffer());
-      serverOut.flush();
-
-      int bytesRead;
-      byte[] buffer = new byte[BUFFER_SIZE];
-      while ((bytesRead = serverIn.read(buffer)) > 0) {
-        clientOut.write(buffer, 0, bytesRead);
-        clientOut.flush();
-      }
-      System.out.printf("Done. Active Thread: %d\r", Thread.activeCount());
-
-      serverSocket.close();
-      clientSocket.close();
-    } catch (SocketException e) {
-      System.out.printf("Client closed connection.\n");
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (MalformedRequestException e) {
       try {
+        HttpRequest clientRequest = new HttpRequest(clientSocket.getInputStream());
+        BufferedOutputStream clientOut = new BufferedOutputStream(clientSocket.getOutputStream(), BUFFER_SIZE);
+        System.out.printf("[%s] %s\n", clientSocket.getInetAddress().getCanonicalHostName(), clientRequest);
+
+        Socket serverSocket = new Socket(clientRequest.getHost(), 80);
+
+        BufferedInputStream serverIn = new BufferedInputStream(serverSocket.getInputStream(), BUFFER_SIZE);
+        BufferedOutputStream serverOut = new BufferedOutputStream(serverSocket.getOutputStream(), BUFFER_SIZE);
+        serverOut.write(clientRequest.toByteBuffer());
+        serverOut.flush();
+
+        int bytesRead;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        while ((bytesRead = serverIn.read(buffer)) > 0) {
+          clientOut.write(buffer, 0, bytesRead);
+          clientOut.flush();
+        }
+        System.out.printf("Done. Active Thread: %d\r", Thread.activeCount());
+
+        serverSocket.close();
+        clientSocket.close();
+      } catch (SocketException e) {
+        System.out.printf("Client closed connection.\n");
+      } catch (UnknownHostException e) {
+        clientSocket.getOutputStream().write("HTTP/1.0 502 Bad Gateway\r\n\r\n".getBytes());
+        clientSocket.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (MalformedRequestException e) {
         clientSocket.getOutputStream().write(e.getMessage().getBytes());
         clientSocket.close();
-      } catch (IOException ex) {
-        ex.printStackTrace();
       }
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
   }
 }
