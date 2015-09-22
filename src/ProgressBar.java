b83647bf2347b8b3554ad382d121272d1a116e00
Liu Xinan <xinan@me.com>
2015-09-22 15:44:39 +0800

    Restyle `ProgressBar` and return `true`/`false` for `update()`

--- src/ProgressBar.java
+++ src/ProgressBar.java
@@ -24,9 +24,10 @@ public class ProgressBar {
     System.out.print(getBar());
   }
 
-  public void update(int amount) {
+  public boolean update(int amount) {
     current += amount;
     System.out.print(getBar());
+    return current == size;
   }
 
   private String getBar() {
@@ -37,22 +38,24 @@ public class ProgressBar {
       numCols = 80;
     }
 
-    char[] equals = new char[numCols - 6];
-    char[] spaces = new char[numCols - 6];
+    numCols -= 20; // Leave space for other text.
+
+    char[] equals = new char[numCols];
+    char[] spaces = new char[numCols];
     Arrays.fill(equals, '=');
-    Arrays.fill(spaces, ' ');
+    Arrays.fill(spaces, '-');
 
     int percentage;
-    if (size == -1) {
+    if (size == -1) { // If no Content-Length available, always show 100%.
       percentage = 100;
     } else {
       percentage = (int) (current * 100F / size);
     }
-    int numEquals = (int) (percentage / 100F * (numCols - 6));
-    int numSpaces = numCols - 6 - numEquals;
+    int numEquals = (int) (percentage / 100F * numCols);
+    int numSpaces = numCols - numEquals;
     int remainingCount = (percentage == 100) ? Thread.activeCount() - 3 : Thread.activeCount() - 2;
     String processed = new String(equals, 0, numEquals);
     String remaining = new String(spaces, 0, numSpaces);
-    return String.format("|%s%s%3d%%| Remaining: %d\r", processed, remaining, percentage, remainingCount);
+    return String.format("|%s%s%3d%%|Remaining: %2d|\r", processed, remaining, percentage, remainingCount);
   }
 }
